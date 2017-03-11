54b84947f9d958882dc409897ab577355dc3d7dc
AntoineGiraud <antoine.giraud@outlook.com>
2017-03-11 00:23:31 -0500

    base interaction DB

---
+++ server/conf.dist.php
@@ -0,0 +1,10 @@
+<?php
+// Exemple de ce qui est attendu dans votre fichier de configuration conf.php
+return [
+    'confSQL' => [
+        'sql_host' => "localhost",
+        'sql_db'   => "hackqc17",
+        'sql_user' => "root",
+        'sql_pass' => ""
+    ]
+];
